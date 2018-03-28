a3705853b7a04ce460ef31a819daf3bfc73f6365
Sergio Cabrera <sergio.cabrera.hernandez@gmail.com>
2018-03-28 20:47:08 +0100

    Initial Commit

---
+++ Mementos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mementos
+//
+//  Created by Sergio Cabrera Hernández on 28/3/18.
+//  Copyright © 2018 Sergio Cabrera Hernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
